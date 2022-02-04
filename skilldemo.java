42bc12026c9497b6de6e0467f7ecac52316e8235
Cat1029 <44483272+Cat1029@users.noreply.github.com>
2022-02-04 12:55:58 -0800

    fail

--- skilldemo.java
+++ skilldemo.java
@@ -1,2 +1,10 @@
 public class skilldemo{
+    public static boolean even(int x){
+        if(x%2==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
---
+++ skilldemotest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class skilldemotest {
+    @Test
+    public void even(){
+        assertEquals(true, skilldemo.even(1));
+    }
+}
